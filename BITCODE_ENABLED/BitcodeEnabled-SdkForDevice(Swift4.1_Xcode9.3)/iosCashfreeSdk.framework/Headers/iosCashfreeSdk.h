6dc6d659c64115be9eaf812246e971e65a6e42f7
Basil M <basil@gocashfree.com>
2018-04-06 16:11:01 +0530

    Add bitcode enabled SDKs in a folder for 3 versions of Xcode

---
+++ BITCODE_ENABLED/BitcodeEnabled-SdkForDevice(Swift4.1_Xcode9.3)/iosCashfreeSdk.framework/Headers/iosCashfreeSdk.h
@@ -0,0 +1,19 @@
+//
+//  iosCashfreeSdk.h
+//  iosCashfreeSdk
+//
+//  Created by Basil M Kuriakose on 27/12/17.
+//  Copyright © 2017 Cashfree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iosCashfreeSdk.
+FOUNDATION_EXPORT double iosCashfreeSdkVersionNumber;
+
+//! Project version string for iosCashfreeSdk.
+FOUNDATION_EXPORT const unsigned char iosCashfreeSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iosCashfreeSdk/PublicHeader.h>
+
+
